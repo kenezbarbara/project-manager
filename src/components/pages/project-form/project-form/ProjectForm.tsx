101ba06cefb036d9e358e60a338cf52677ebfb44
kenezbarbara <kenez.barbara96@gmail.com>
2025-07-01 18:18:34 +0200

    implement progressBar logic

--- src/components/pages/project-form/project-form/ProjectForm.tsx
+++ src/components/pages/project-form/project-form/ProjectForm.tsx
@@ -3,6 +3,8 @@ import './ProjectForm.css'
 import FormButton from '../form-button/FormButton'
 import { useState } from 'react'
 import ProjectInfoStep from '../project-info-step/ProjectInfoStep'
+import TeamMembersStep from '../team-member-step/TeamMembersStep'
+import LinksStep from '../link-step/LinksStep'
 
 export interface ProjectFormValues {
   projectName: string
@@ -15,14 +17,29 @@ export default function ProjectForm() {
     projectDescription: '',
   }
 
+  const [currentStep, setCurrentStep] = useState<number>(1)
+
   const [formValues, setFormValues] =
     useState<ProjectFormValues>(initialFormValues)
+
+  const renderStep = () => {
+    switch (currentStep) {
+      case 1:
+        return <ProjectInfoStep setFormValues={setFormValues} />
+      case 2:
+        return <TeamMembersStep />
+      case 3:
+        return <LinksStep />
+      default:
+        return <ProjectInfoStep setFormValues={setFormValues} />
+    }
+  }
 
   return (
     <div>
       <div className="project-form-header">
-        <ProgressBar />
-        <ProjectInfoStep setFormValues={setFormValues} />
+        <ProgressBar currentStep={currentStep} />
+        {renderStep()}
         <div className="btn-container">
           <FormButton text="Back" color="#212529" />
           <FormButton text="Continue" color="#0dcaf0" />
--- src/components/progress-bar/ProgressBar.tsx
+++ src/components/progress-bar/ProgressBar.tsx
@@ -1,8 +1,23 @@
-export default function ProgressBar() {
+export default function ProgressBar({ currentStep }: { currentStep: number }) {
+  const renderPercentage = () => {
+    switch (currentStep) {
+      case 1:
+        return 0
+      case 2:
+        return 33
+      case 3:
+        return 66
+      default:
+        return 0
+    }
+  }
   return (
     <div className="progress" role="progressbar" style={{ height: '20px' }}>
-      <div className="progress-bar bg-info" style={{ width: `${100}%` }}>
-        {0}%
+      <div
+        className="progress-bar bg-info"
+        style={{ width: `${renderPercentage()}%` }}
+      >
+        {renderPercentage()}%
       </div>
     </div>
   )
