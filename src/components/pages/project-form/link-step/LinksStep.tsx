0499171122c7f2877ea73cf8b793e6bd77992254
kenezbarbara <kenez.barbara96@gmail.com>
2025-07-01 16:04:03 +0200

    implement link addition and display on LinkStep

--- src/components/pages/project-form/link-step/LinksStep.tsx
+++ src/components/pages/project-form/link-step/LinksStep.tsx
@@ -1,7 +1,26 @@
+import { useState } from 'react'
 import FormButton from '../form-button/FormButton'
 import './LinksStep.css'
 
 export default function LinksStep() {
+  const [selectedLink, setSelectedLink] = useState<string>('')
+  const [links, setLinks] = useState<string[]>([])
+
+  const handleAddLink = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    const newLink = selectedLink
+    if (newLink.trim() !== '') {
+      // Check if the link is not empty
+      if (links.includes(newLink)) {
+        alert('This link has already been added.')
+        return
+      }
+      // Check if the link already exists
+      setLinks((prevLinks) => [...prevLinks, newLink])
+      setSelectedLink('')
+    }
+  }
+
   return (
     <form className="link-form">
       <h5>As a final step, add links to your project</h5>
@@ -13,9 +32,23 @@ export default function LinksStep() {
         placeholder="https://example.com"
         pattern="https://.*"
         size={30}
-        required
+        onChange={(e) => {
+          setSelectedLink(e.target.value)
+        }}
+        value={selectedLink}
       />
-      <FormButton text="Add link" color="#848789" />
+      <FormButton text="Add link" color="#848789" onClick={handleAddLink} />
+      <div className="added-link-container">
+        <h4>Added links</h4>
+        <div className="link-list">
+          {links.map((link) => (
+            <div className="link-box" key={link}>
+              <a href="">{link}</a>
+              <button className="remove-link-btn">x</button>
+            </div>
+          ))}
+        </div>
+      </div>
     </form>
   )
 }
--- src/components/pages/project-form/project-form/ProjectForm.tsx
+++ src/components/pages/project-form/project-form/ProjectForm.tsx
@@ -1,14 +1,14 @@
 import ProgressBar from '@/components/progress-bar/ProgressBar'
 import './ProjectForm.css'
 import FormButton from '../form-button/FormButton'
-import TeamMembersStep from '../team-member-step/TeamMembersStep'
+import LinksStep from '../link-step/LinksStep'
 
 export default function ProjectForm() {
   return (
     <div>
       <div className="project-form-header">
         <ProgressBar />
-        <TeamMembersStep />
+        <LinksStep />
         <div className="btn-container">
           <FormButton text="Back" color="#212529" />
           <FormButton text="Continue" color="#0dcaf0" />
