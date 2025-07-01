dc249f0fdcfa5fdc86853c10d22a641df75a007d
kenezbarbara <kenez.barbara96@gmail.com>
2025-07-01 18:06:38 +0200

    form values base event handling for ProjectInfoStep

--- src/components/pages/project-form/project-form/ProjectForm.tsx
+++ src/components/pages/project-form/project-form/ProjectForm.tsx
@@ -1,14 +1,28 @@
 import ProgressBar from '@/components/progress-bar/ProgressBar'
 import './ProjectForm.css'
 import FormButton from '../form-button/FormButton'
-import LinksStep from '../link-step/LinksStep'
+import { useState } from 'react'
+import ProjectInfoStep from '../project-info-step/ProjectInfoStep'
+
+export interface ProjectFormValues {
+  projectName: string
+  projectDescription: string
+}
 
 export default function ProjectForm() {
+  const initialFormValues: ProjectFormValues = {
+    projectName: '',
+    projectDescription: '',
+  }
+
+  const [formValues, setFormValues] =
+    useState<ProjectFormValues>(initialFormValues)
+
   return (
     <div>
       <div className="project-form-header">
         <ProgressBar />
-        <LinksStep />
+        <ProjectInfoStep setFormValues={setFormValues} />
         <div className="btn-container">
           <FormButton text="Back" color="#212529" />
           <FormButton text="Continue" color="#0dcaf0" />
--- src/components/pages/project-form/project-info-step/ProjectInfoStep.tsx
+++ src/components/pages/project-form/project-info-step/ProjectInfoStep.tsx
@@ -1,13 +1,40 @@
+import type { Dispatch, SetStateAction } from 'react'
 import './ProjectInfoStep.css'
+import type { ProjectFormValues } from '../project-form/ProjectForm'
 
-export default function ProjectInfoStep() {
+interface ProjectInfoStepProps {
+  setFormValues: Dispatch<SetStateAction<ProjectFormValues>>
+}
+
+const ProjectInfoStep: React.FC<ProjectInfoStepProps> = ({ setFormValues }) => {
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target
+    setFormValues((prevValues: ProjectFormValues) => ({
+      ...prevValues,
+      [name]: value,
+    }))
+  }
+
   return (
     <form className="project-form" action="">
       <h5>Enter your project details as a first step</h5>
       <label htmlFor="projectName">Your project name</label>
-      <input type="text" id="projectName" name="projectName" />
+      <input
+        type="text"
+        id="projectName"
+        name="projectName"
+        onChange={handleInputChange}
+      />
       <label htmlFor="projectDescription">Detailed description</label>
-      <textarea name="projectDescription" id="projectDescription"></textarea>
+      <textarea
+        name="projectDescription"
+        id="projectDescription"
+        onChange={handleInputChange}
+      ></textarea>
     </form>
   )
 }
+
+export default ProjectInfoStep
