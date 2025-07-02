40d1758a1b6c25403882ff3a8476a67b93b9ed11
kenezbarbara <kenez.barbara96@gmail.com>
2025-07-02 14:19:01 +0200

    complete ProjectCard filtering on ProjectList

--- src/components/pages/project-list/ProjectList.tsx
+++ src/components/pages/project-list/ProjectList.tsx
@@ -1,11 +1,12 @@
 import { Button, ProjectCard } from '@/components'
 import { getAllProjects } from '@/services/dataService'
 import type { Project } from '@/types'
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { useNavigate } from 'react-router'
 
 export default function ProjectList() {
-  const [projects, setProjects] = useState<Array<Project>>([])
+  const [allProjects, setAllProjects] = useState<Array<Project>>([])
+  const [searchTerm, setSearchTerm] = useState<string>('')
   const navigate = useNavigate()
 
   const handleNewProject = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -16,43 +17,71 @@ export default function ProjectList() {
   }
 
   const loadProjects = () => {
-    getAllProjects().then((resp) => setProjects(resp))
+    getAllProjects().then((resp) => setAllProjects(resp))
   }
 
   useEffect(() => {
     loadProjects()
   }, [])
 
+  const displayedProjects = useMemo(() => {
+    if (!searchTerm.trim()) {
+      return allProjects // If the search text is empty, display all projects
+    }
+
+    const lowerCaseSearchTerm = searchTerm.trim().toLowerCase()
+    return allProjects.filter(
+      (project) =>
+        project.name.toLowerCase().includes(lowerCaseSearchTerm) ||
+        project.description.toLowerCase().includes(lowerCaseSearchTerm)
+    )
+  }, [allProjects, searchTerm])
+
+  const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value)
+  }
+
   return (
     <div className="row flex-grow-1 justify-content-center align-items-start ">
-      {projects.length > 0 && (
-        <div className="col text-center mt-5 p-5 bg-light rounded-2 bg-opacity-50">
-          <h1>All Corporate Projects</h1>
-          <h6>browse company projects or add a new project easily</h6>
-          <form className="d-flex justify-content-center" role="search">
-            <input
-              className="form-control me-2 mt-2"
-              type="search"
-              placeholder="Search for projects..."
-              aria-label="Search"
-              style={{ width: '40%' }}
-            />
-            <Button text="Search" iconClass="bi bi-search" />
-          </form>
-        </div>
-      )}
+      <div className="col text-center mt-5 p-5 bg-light rounded-2 bg-opacity-50">
+        <h1>All Corporate Projects</h1>
+        <h6>browse company projects or add a new project easily</h6>
+        <form className="d-flex justify-content-center" role="search">
+          <input
+            className="form-control me-2 mt-2"
+            type="search"
+            placeholder="Search for projects..."
+            aria-label="Search"
+            style={{ width: '40%' }}
+            value={searchTerm}
+            onChange={handleSearchInputChange}
+          />
+          <i className="bi bi-search ms-2 fs-2"></i>
+        </form>
+      </div>
 
-      <div className="row mx-auto row-cols-1 row-cols-md-2 row-cols-xl-3 g-5 my-5">
-        {projects.map((project) => (
+      <div className="d-flex justify-content-center row row-cols-1 row-cols-md-2 row-cols-xl-3 g-5 my-5">
+        {displayedProjects.map((project) => (
           <ProjectCard
             key={project.id}
             name={project.name}
             description={project.description}
           />
         ))}
+        {/* Show message if no match found*/}
+        {searchTerm && displayedProjects.length === 0 && (
+          <div
+            className="w-100 d-flex justify-content-center align-items-center"
+            style={{ minHeight: '10rem' }}
+          >
+            <div className="text-center bg-light bg-opacity-50 rounded-2 p-3 mx-auto">
+              <p>No projects found matching "{searchTerm}".</p>
+            </div>
+          </div>
+        )}
       </div>
       <div className="col text-center my-5 p-0 bg-light rounded-2 bg-opacity-50">
-        {projects.length > 0 ? (
+        {allProjects.length > 0 ? (
           <>
             <h4 className="mt-3">Can't find the project you're looking for?</h4>
             <h6>Start adding projects!</h6>
