59d8016bce3fb33e7c6798e93f032648ef03db22
kenezbarbara <kenez.barbara96@gmail.com>
2025-07-02 18:52:47 +0200

    improve colors on ProjectCard, ProjectLinkList and TeamMemberList

--- src/components/project-card/ProjectCard.tsx
+++ src/components/project-card/ProjectCard.tsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router'
-import './ProjectCard.css'
 
 interface ProjectCardProps {
   name: string
@@ -28,7 +27,7 @@ export default function ProjectCard({
       <Link to={to} className="text-decoration-none">
         <div className="card text-center shadow mx-auto mx-md-3 mx-lg-5">
           <img src={imageUrl} alt={name} />
-          <div className="card-body text-white card-bg-color ">
+          <div className="card-body text-white bg-secondary">
             <h5 className="card-title ">{name}</h5>
             <p
               className="card-text d-inline-block text-truncate"
